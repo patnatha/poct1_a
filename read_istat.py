baa7ba638aba65a726a9b4856298a65f39733dbe
Nathan Patel <nthnpatel@gmail.com>
2022-03-08 15:02:18 -0500

    first commit

---
+++ read_istat.py
@@ -0,0 +1,254 @@
+import socket 
+import pprint
+import json
+import re
+import redcap as rc
+import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
+
+pp = pprint.PrettyPrinter(indent=4)
+
+def pretty_print(xmlDoc):
+    print(BeautifulSoup(ET.tostring(xmlDoc), 'xml').prettify())
+
+def get_msg(cs):
+    #Define parsing variables
+    curInd = -1
+    startInd = None
+    startTag = None
+    endTag = None
+    wholeMsg = ""
+
+    while True:
+        #REad one byte at a time
+        byte = cs.recv(1).decode("utf-8", errors='ignore')
+        curInd = curInd + 1
+        wholeMsg = wholeMsg + byte
+        
+        #Find start of first tag
+        if(startTag == None and startInd == None and byte == "<"):
+            startInd = curInd
+        #Find end of first tag
+        if(startTag == None and startInd != None and byte == ">"):
+            startTag = wholeMsg[startInd:curInd+1]
+            endTag = startTag[:1] + '/' + startTag[1:]
+        
+        #End of message when close tag found
+        if(startTag != None and endTag != None and wholeMsg.find(endTag) > -1):
+            parsedMsg = ET.fromstring(wholeMsg)
+            return(parsedMsg)
+
+def get_hdr(parMsg):
+    for child in parMsg:
+        if(child.tag == "HDR"):
+            return(ET.tostring(child))
+    return None
+
+def get_device(parMsg):
+    device_id = None
+    vendor_id = None
+    model_id = None
+    serial_id = None
+    for child in parMsg:
+        if(child.tag == "DEV"):
+            for children in child:
+                if(children.tag == "DEV.device_id"):
+                    device_id = children.get("V")
+                if(children.tag == "DEV.vendor_id"):
+                    vendor_id = children.get("V")
+                if(children.tag == "DEV.model_id"):
+                    model_id = children.get("V")
+                if(children.tag == "DEV.serial_id"):
+                    serial_id = children.get("V")
+
+    return(vendor_id, model_id, device_id, serial_id)
+
+def get_obs(cs, hdr):
+    msg = "<REQ.R01>"
+    msg = msg + hdr.decode("utf-8")
+    msg = msg + "<REQ><REQ.request_cd V=\"ROBS\"/></REQ>"
+    msg = msg + "</REQ.R01>"
+    if(cs.sendall(msg.encode("utf-8")) == None):
+        robs = get_msg(cs)
+        return(robs)
+    else: 
+        print("REQ.R01 error")
+        return None
+
+def extract_obs(observation):
+    data = []
+    rgt_data = {}
+    patient_id = None
+    obs_date = None
+    temp_correct = False
+    pt_temp = None
+    for child in observation[1]:
+        if(child.tag == "SVC.observation_dttm"):
+            obs_date = child.get("V")
+        elif(child.tag == "PT"):
+            for children in child:
+                if(children.tag == "PT.patient_id"):
+                    patient_id = children.get("V")
+                elif(children.tag == "OBS"):
+                    obs_type = None
+                    obs_value = None
+                    obs_unit = None
+                    obs_nte = None
+                    for childrens in children:
+                        if(childrens.tag == "OBS.observation_id"):
+                            obs_type = childrens.get("V")
+                        elif(childrens.tag == "OBS.value"):
+                            obs_value = childrens.get("V")
+                            obs_unit = childrens.get("U")
+                        elif(childrens.tag == "NTE"):
+                            for childrenss in childrens:
+                                if(childrenss.tag == "NTE.text"):
+                                    if(re.match('^\d+(\.\d+)?$',childrenss.get("V")) is not None):
+                                        obs_nte = float(childrenss.get("V"))
+                    data.append([obs_type, obs_value, obs_unit, obs_nte])
+        elif(child.tag == "RSN"):
+            for children in child:
+                if(children.tag == "RSN.apoc_prompt"):
+                    if(children.get("V") == "PT Temp F"):
+                        temp_correct = True
+                elif(children.tag == "RSN.apoc_content"):
+                    pt_temp = children.get("V")
+        elif(child.tag == "RGT"):
+            for children in child:
+                if(children.tag == "RGT.name"):
+                    rgt_data["name"] = children.get("V")
+                elif(children.tag == "RGT.lot_number"):
+                    rgt_data["lot_number"] = children.get("V")
+                elif(children.tag == "RGT.expiration_date"):
+                    rgt_data["expiration_date"] = children.get("V")
+
+    dataout = []
+    temp_append = None
+    for datum in data:
+        dataout.append({"name": patient_id, "datetime": obs_date, 
+            "type": datum[0], "value": datum[1], "units": datum[2], 
+            "note": datum[3]})
+
+        #Get temp corrected values for CG4
+        if(("CG8" in rgt_data["name"] or 
+                "CG4" in rgt_data["name"]) and datum[3] != None):
+            dataout[len(dataout) - 1]["type"] = "temp_" + dataout[len(dataout) - 1]["type"] 
+
+    if(temp_correct):
+        dataout.append({"name": patient_id, "datetime": obs_date, 
+            "type": "temp", "value": pt_temp, 
+            "units": "fahrenheit", "note": None})
+
+    return ET.tostring(observation[0]), dataout, rgt_data
+
+def parse_device_status(themsg):
+    #Get the number of observations to sent
+    unsentObs = None
+    try:
+        for child in themsg:
+            if(child.tag == "DST"):
+                for children in child:
+                    if(children.tag == "DST.new_observations_qty"):
+                        unsentObs = int(children.get("V"))
+    except:
+        unsentObs = 0
+
+    return(unsentObs)
+
+def ack(cs, hdr):
+    #Extract the ctrl id from the header
+    ctrlid = None
+    parsedHDR = ET.fromstring(hdr)
+    for child in parsedHDR:
+        if(child.tag == "HDR.control_id"):
+            ctrlid = child.get("V")
+            break
+
+    #Build the ack message
+    msg = '<ACK.R01>'
+    msg = msg + hdr.decode("utf-8")
+    msg = msg+'<ACK>'
+    msg = msg+'<ACK.type_cd V="AA" /><ACK.ack_control_id V="' + ctrlid + '" />'
+    msg = msg+'</ACK>'
+    msg = msg+'</ACK.R01>'
+
+    if(cs.sendall(msg.encode('utf-8')) != None):
+        print("ACK error")
+
+def is_terminated(themsg):
+    if(themsg.tag == "END.R01"):
+        print(ET.tostring(themsg))
+        reason = None
+        for child in themsg:
+            if(child.tag == "TRM"):
+                for children in child:
+                    if(children.tag == "TRM.reason_cd"):
+                        reason = children.get("V")
+        print("Terminated Connection, " + str(reason))
+        return True
+    return False
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind(('0.0.0.0', 8080))
+serversocket.listen(1)
+
+while True:
+    #accept connections from outside
+    (clientsocket, address) = serversocket.accept()
+   
+    #Parse the hello message
+    msg = get_msg(clientsocket)
+    if(is_terminated(msg)): continue
+    hdr = get_hdr(msg)
+    vendor, model, device, serial = get_device(msg)
+    ack(clientsocket, hdr)
+    print("Ready to send results from:", vendor, model, device, serial)
+
+    #Parse device status message
+    msg = get_msg(clientsocket)
+    if(is_terminated(msg)): continue
+    hdr = get_hdr(msg) 
+    ack(clientsocket, hdr)
+    obs_to_send = parse_device_status(msg)
+    print("Number of unsent records:", obs_to_send)
+
+    #Get unsent observations
+    for get_it in range(obs_to_send):
+        #Request the next object
+        obs = get_obs(clientsocket, hdr)
+        if(is_terminated(obs)): break
+        hdr, data, test_info = extract_obs(obs)
+
+        #Build the test info struct yo post
+        test_info["vendor"] = vendor
+        test_info["model"] = model
+        test_info["device"] = device
+        test_info["serial"] = serial
+        test_info["json_data"] = json.dumps(data)
+        #pp.pprint(data)
+     
+        #Build the record for a given test type for posting
+        dataoutput = {"name": data[0]["name"]}
+        dataoutput["datetime"] = rc.parse_value("datetime", data[0]["datetime"])
+        for item in data:
+            dataoutput[rc.convert_colname(item["type"])] = rc.parse_value(item["type"],item["value"])
+        pp.pprint(dataoutput)
+
+        #Check that there is not empty fields
+        to_cont = True
+        for item in dataoutput:
+            if(dataoutput[item] == None): 
+                to_cont = False
+
+        #Acknowledge the upload
+        if(to_cont):
+            post_test_table = rc.post_redcap(dataoutput, 
+                    rc.which_table(test_info["name"]))
+            post_test_info = rc.post_redcap(test_info, 
+                    rc.which_table("TEST_INFO"))
+            print("Sucessful Posting:", post_test_info, post_test_table)
+        ack(clientsocket, hdr)
+
+    #Close the socket connection
+    clientsocket.close()
+
---
+++ redcap.py
@@ -0,0 +1,151 @@
+import requests
+import json
+import sys
+from datetime import datetime
+
+postUrl = "https://redcap.wakehealth.edu/redcap/api/"
+token_file = "/home/pi/Documents/abaxis_istat/token.auth"
+labs_token = None
+chem8_token = None
+cg4_token = None
+
+def load_tokens():
+    global labs_token
+    global chem8_token
+    global cg4_token
+    f = open(token_file)
+    for line in f:
+        parsed = (line.strip("\n").split(":"))
+        if(len(parsed) == 2):
+            #print(parsed)
+            if(parsed[0] == 'labs'):
+                labs_token = parsed[1]
+            elif(parsed[0] == 'chem8'):
+                chem8_token = parsed[1]
+            elif(parsed[0] == 'cg4'):
+                cg4_token = parsed[1]
+    f.close()
+load_tokens()
+
+def convert_int(theVal):
+    if(theVal == None):
+        return(None)
+    else:
+        try:
+            return(str(int(theVal)))
+        except Exception as err:
+            print(err)
+            return(None)
+
+def convert_one_decimal(theVal):
+    if(theVal == None):
+        return(None)
+    else:
+        try:
+            return(str("{:.1f}".format(theVal)))
+        except Exception as err:
+            print("convert_one_decimal:",err)
+            return(None)
+
+def convert_two_decimal(theVal):
+    if(theVal == None):
+        return(None)
+    else:
+        try:
+            return(str("{:.2f}".format(theVal)))
+        except Exception as err:
+            print("convert_two_decimal:", err)
+            return(None)
+
+def convert_three_decimal(theVal):
+    if(theVal == None):
+        return(None)
+    else:
+        try:
+            return(str("{:.3f}".format(theVal)))
+        except Exception as err:
+            print("convert_three_decimal:", err)
+            return(None)
+
+def convert_colname(theVal):
+    theVal = theVal.lower()
+    theVal = theVal.replace("*", "")
+    theVal = theVal.replace(",", "_")
+    return(theVal)
+
+def parse_value(theCol, theVal):
+    try:
+        theCol = convert_colname(theCol)
+        if(theCol == "angap" or theCol == "bun" or theCol == "cl" or \
+            theCol == "glu" or theCol == "hct" or theCol == "na" or theCol == "tco2"):
+            return convert_int(theVal)
+        elif(theCol == "crea" or theCol == "k" or theCol == "hb"):
+            return convert_one_decimal(float(theVal))
+        elif(theCol == "ica"):
+            return convert_two_decimal(float(theVal))
+        elif(theCol == "lac"):
+            return convert_two_decimal(float(theVal))
+        elif(theCol == "be_ecf" or theCol == "tco2" or theCol == "temp_po2" or 
+                theCol == "po2" or theCol == "so2"):
+            return convert_int(theVal)
+        elif(theCol == "pco2" or 
+                theCol == "temp_pco2" or theCol == "hco3"):
+            return convert_one_decimal(float(theVal))
+        elif(theCol == "ph" or theCol == "temp_ph"):
+            return convert_three_decimal(float(theVal))
+        elif(theCol == "datetime"):
+            datetime_object = datetime.strptime(theVal, "%Y-%m-%dT%H:%M:%S.%f")
+            return datetime_object.strftime("%Y-%m-%d %H:%M:%S")
+        elif(theCol == "name" or theCol == "temp"):
+            return(theVal)
+    except Exception as err:
+        print("parse_value:", err)
+    return None
+
+def post_redcap(theDatas, which_token):
+    try:
+        data = {
+            'token': which_token,
+            'content': 'record',
+            'action': 'import',
+            'format': 'json',
+            'type': 'flat',
+            'overwriteBehavior': 'normal',
+            'forceAutoNumber': 'true',
+            'data': '',
+            'returnContent': 'count',
+            'returnFormat': 'json'
+        }
+
+        theSendStruct = {'record_id': 0}
+        for key in theDatas:
+            theSendStruct[key] = theDatas[key]
+        data['data'] = json.dumps([theSendStruct])
+
+        r = requests.post(postUrl, data=data)
+
+        if(r.status_code != 200):
+            print(r.json())
+            return(-2)
+        elif(r.status_code == 200 and r.json()['count'] != 1):
+            print(r.json())
+            return(-1)
+        else:
+            return(1)
+    except Exception as err:
+        print("post_redcap:", err)
+        log_later(theDatas)
+        return(-3)
+
+def which_table(test_name):
+    if("CHEM8" in test_name):
+        return chem8_token
+    elif("CG4" in test_name):
+        return cg4_token
+    elif("TEST_INFO" in test_name):
+        return labs_token
+    else:
+        print("which_table (" + test_name + "): Doesn't exists")
+        #sys.exit(1)
+
+    return None
